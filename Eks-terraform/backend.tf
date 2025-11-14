5f8057fd036e6935fa92763af02c675ae959bfb9
root <root@ip-172-31-45-67.ap-northeast-1.compute.internal>
2025-11-14 16:51:31 +0000

    Add complete K8s + Jenkins CI/CD project (Tetris-V2 + Jenkinsfile + Terraform + Docker)

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "mario123bucket" # Replace with your actual S3 bucket name
+    bucket = "bhargavvb" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
-    region = "ap-south-1"
+    region = "ap-northeast-1"
+    use_lockfile = true 
   }
 }
